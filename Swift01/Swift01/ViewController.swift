035c2adee4574247cafc87f31a44e5f19bc4702f
Daves_Code_Hou <840737320@qq.com>
2017-05-21 20:23:21 +0800

    创建第一个项目

---
+++ Swift01/Swift01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift01
+//
+//  Created by Code_Hou on 2017/5/21.
+//  Copyright © 2017年 侯森魁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
